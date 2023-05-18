84cd5c0d6f4bf3311ab107712bedae98f01b5045
Pablo González Segarra <pablogscraft@gmail.com>
2023-05-18 08:43:28 +0200

    All working!

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from src.bootloader import *
 from test import *
 
 def main():
-    upload_code("../HUPVB-Script/test_codes/blink.bin")
+    upload_code(r"HUPVB-Script\test_codes\blink.bin")
     
 main()
--- src/bootloader.py
+++ src/bootloader.py
@@ -5,8 +5,9 @@ from enum import Enum
 from tqdm import tqdm
 from typing import List
 
+
 VERBOSE = False
-TIMEOUT_MS = 20000
+TIMEOUT_MS = 50000
 UPDATE_PERIOD_S = 0.001
 
 class Commands(Enum):
@@ -27,7 +28,8 @@ class ResponseStatus(Enum):
     ERROR=1
     BAD_PARAMETERS=2
     
-def upload_code(file_path : str) :
+def upload_code(file_path) :
+    print(file_path)
     print(f"- 👨‍💻 Upload code from file: {file_path}")
     print(f"|    Bootloader version : {get_version()}")
     print("|    Erasing memory")
@@ -37,20 +39,34 @@ def upload_code(file_path : str) :
 
     file = open(file=file_path, mode='rb')
     end = False
-    pbar = tqdm(range(7), colour='green', leave=True)#Progress bar chula chula
+
+    data = file.read()
+    varible = len(data)
+    print(varible)
+    print(6*32768)
+    if (varible > (6*32768*4)):
+        print(f"|    The file is too big")
+        return 
+    
+    pbar = tqdm(range(7), colour='green', leave=True, position=0)#Progress bar chula chula
     for i in pbar:
         pbar.set_description("Sector " + str(i) + " of 6")
-
-        data = file.read(32768)
+        # bytes = file.read(32768)
+        # data: List[int] = [int(byte) for byte in bytes]
+            
         #Si hemos llegado al final del archivo, rellenamos con 0xff
-        if len(data) < 32768:
+        aux = len(data)
+        print(aux)
+        if aux < 32768*4:
             end = True
-            map(lambda x: x if x != '' else 0xff, data)
+            data += b'\xff' * (32768*4 - len(data))
 
-        write_memory(i, data)
+        write_memory(i, data[0:32768*4])
+        data = data[32768*4:]
 
         if end:
             break
+       
 
     remaining_data = file.read()
     file.close()
@@ -66,11 +82,12 @@ def get_version() -> int :
     __initialize_can()
     send_message(Message(int(Commands.GET_VERSION.value), []))
 
-    msg = __wait_for_bootloader_command_response()
     __wait_for_bootloader_message(matches_first_byte=Commands.ACK.value)
-    
+    msg = __wait_for_bootloader_message()
+    __wait_for_bootloader_message(matches_first_byte=Commands.ACK.value)
+
     __unitialize_can()
-    return msg[0]
+    return msg.data[0]
 
 def read_memory(sector: int) -> List[int]:
     if((sector < 0) or (sector > 7)):
@@ -109,24 +126,25 @@ def write_memory(sector : int, data : List[int]) :
     send_message(Message(int(Commands.WRITE_MEMORY.value), [sector]))
 
     __wait_for_bootloader_message(matches_first_byte=Commands.ACK.value)
-    send_message(Message(int(Commands.ACK.value), []))
+
+    send_message(Message(int(Commands.WRITE_MEMORY.value), [int(Commands.ACK.value)]))
 
     packets_left = 2048
     counter = 1
-    index = 0
     while packets_left > 0:
-        send_message(Message(int(Commands.ACK.value), [data[index:64]]))
+        send_message(Message(int(Commands.WRITE_MEMORY.value),data[:64]))
         data = data[64:]
 
         if counter >= 8:
-            __wait_for_bootloader_message(matches_first_byte=Commands.ACK.value)
-            send_message(Message(int(Commands.ACK.value), []))
+            msg = __wait_for_bootloader_message(matches_first_byte=Commands.ACK.value)
+            send_message(Message(int(Commands.WRITE_MEMORY.value), [int(Commands.ACK.value)]))
+
             counter = 1
         else:
             counter += 1
 
         packets_left -= 1
-        index += 64
+       
 
     __wait_for_bootloader_message(matches_first_byte=Commands.ACK.value)
     __unitialize_can()
@@ -134,8 +152,9 @@ def write_memory(sector : int, data : List[int]) :
 def erase_memory():
     __initialize_can()
     send_message(Message(Commands.ERASE_MEMORY.value, [0x00, 0x06]))    
-    __wait_for_bootloader_command_response()
-    __wait_for_bootloader_command_response()
+    __wait_for_bootloader_message(matches_first_byte=Commands.ACK.value)
+    __wait_for_bootloader_message(matches_first_byte=Commands.ACK.value)
+
     __unitialize_can()
 
 
@@ -170,7 +189,7 @@ def __wait_for_bootloader_message(matches_first_byte=None) -> Message:
             
         if (message and not matches_first_byte) or (message and (message.data[0].value == matches_first_byte)):
             return message
-    
+    print("\nTimed out waiting for bootloader message\n")
     raise BootloaderException()
 
 def __get_current_time_ms() -> int:
